772c70e803404688e8f63c4e7d5135cd9300f6dd
MirajHoque <a.h.miraj2031@gmail.com>
2021-12-23 23:02:48 +0600

    attribute manipulation

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-//select a element inside div
-//text(): replace text.
-//$(".my-div h1").text("hi");
+//get attribute
+//$("a").attr("href");
 
-//slect element based on id
-//$("#p1").text("hello");
+//remove attribute
+//$("a").removeAttr("href");
 
-//select multiple element at once
-//$("h1,p").text("hi");
-
-//html method for using html attribute.
-//$("#p1").html("<b>goodbye</b>")
-
-//append(): insert specified content at the end of the selected element.
-//$("#p1").append("goodbye")
-
-//prepend(): insert specified content at the begaining of the selected element.
-//$("#p1").prepend("goodbye");
-
-var myParagraph = $("<P></P>").text("this is paragraph 0");
-
-//before():insert specified content before the select element.
-//$("#p1").before(myParagraph);
-
-//after(): insert specified content after the select element.
-$("#p1").after(myParagraph);
+//set attribute 
+$("a").attr("href", "http://facebook.com");
