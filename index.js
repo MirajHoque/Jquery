d6c84a811fea990d34674854c60bf945b4e6f7fd
MirajHoque <a.h.miraj2031@gmail.com>
2021-12-25 18:28:15 +0600

    animation

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-$("#logInbutton").click(function(){
-  var password1 = $("#pass1").val();
-  var password2 = $("#pass2").val();
+//hide(): hide the seleted element
 
-  if(password1 != "" & password2 != ""){
-    if(password1 === password2){
-      alert("Successfully Loged In");
-    }
-    else{
-      alert("Password Missmatch");
-    }
-  }
-  else{
-    alert("please enter password");
-  }
-  });
+   //we can use hide() with parameter or without parameter.
+
+   $("#btn").click(function(){
+  //$("#div1").hide(2000)
+ // $("#div1").hide(2000).show(2000);
+
+  //fadeIn(): gradually changes the opacity of seleted element form hidden to visible.
+  //$("#div1").fadeIn(2000);
+
+  //fadeOut(): gradually chanages the opacit of seleted element from visible to hidden.
+  //$("#div1").fadeOut(2000);
+
+  //fadeIn(), fadeOut() together
+  //$("#div1").fadeOut(2000).fadeIn(2000);
+
+  //fadeToggle(): both fadeIn, fadeOut.
+  //$("#div1").fadeToggle(2000);
+
+  //fadeTo(): used for changes the opacity level
+  //opacity level 0-1.
+  //$("#div1").fadeTo(2000, 0.4);
+
+  //slideUp(): slide up selecte elemnts.
+  //$("#div1").slideUp(2000);
+
+  //slideDown(): slide down seleted elements.
+  //$("#div1").slideDown(2000);
+
+  //using both slideUp() and slideDown()
+  $("#div1").slideUp(2000).slideDown(2000);
+})
